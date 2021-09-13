e6006b0356c5dd4bbb9c17eea86ee81cdbc9793a
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-09-13 17:16:24 +0300

    added profile folder

--- photos/admin.py
+++ photos/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Image
+from .models import Follows, Profile, Image,Like,Comment,
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Image)
-
+admin.site.register(Follows)
+admin.site.register(Like)
+admin.site.register(Comment)
 
